b16a935ef4b2183012811cfcfb4e7a823e347f5b
darkstyler <borkokrumov@gmail.com>
2021-02-10 00:10:40 +0200

    Initialize empty project

---
+++ app/src/main/java/com/darkstyler/whatyouknow/util/Prefs.java
@@ -0,0 +1,22 @@
+package com.darkstyler.whatyouknow.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Prefs {
+    private SharedPreferences preferences;
+    public Prefs(Activity activity){
+        this.preferences = activity.getPreferences(Context.MODE_PRIVATE);
+    }
+    public void saveHighestScore(int score){
+        int currentScore = score;
+        int lastScore = preferences.getInt("high_score",0);
+        if(currentScore> lastScore){
+            preferences.edit().putInt("high_score",currentScore).apply();
+        }
+    }
+    public int getHighestScore(){
+        return  preferences.getInt("high_score",0);
+    }
+}
